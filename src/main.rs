cc93ac0f40f8f874bf343dbc9d61c42b9c29822d
Artur Peixoto de Souza Lino <artur.lino@mundoplanalto.com.br>
2025-12-05 15:25:06 -0300

    initialized git

---
+++ src/main.rs
@@ -0,0 +1,307 @@
+use std::{collections::VecDeque, error::Error, ffi::OsStr, fs::{FileType, Metadata}, marker::PhantomData, os::windows::fs::FileTypeExt, path::{Path, PathBuf}, str::FromStr, sync::{Arc, Weak}};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, to_string_pretty};
+use tokio::{io::{AsyncWriteExt, stdout}, join, sync::{RwLock, mpsc::{Sender, UnboundedReceiver, UnboundedSender, channel}}};
+
+
+fn main() {
+    tokio
+    ::runtime
+    ::Builder
+    ::new_multi_thread()
+    .enable_all()
+    .build()
+    .unwrap()
+    .block_on(amain());
+}
+pub async fn amain(){
+    let dir = PathBuf::from_str(".").unwrap();
+    let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
+    let mut crawler = Crawler{elements_sender: sender};
+    let mut writer = Writer{input: receiver};
+    let (_,_) = join!(crawler.crawl(dir.as_path()), writer.run());
+}
+
+impl Writer{
+    pub async fn run(&mut self){
+        let mut stdout = stdout();
+        stdout.write_all(b"[\n").await.unwrap();
+
+        async fn print_element(element: &OwnedFsElement, stdout: &mut tokio::io::Stdout, is_first: &mut bool){
+            if *is_first{
+                *is_first = false;
+            } else {
+                stdout.write_all(b",\n").await.unwrap();
+            }
+            let row =  element.make_row();
+            let serialized = serde_json::to_string(&row).unwrap();
+            stdout.write_all(serialized.as_bytes()).await.unwrap();
+        }
+        let mut is_first = true;
+
+        while let Some(weak_element) = self.input.recv().await{
+            let Some(element) = weak_element else {break;};
+            if let Some(element) = element.upgrade(){
+                print_element(&element, &mut stdout, &mut is_first).await;
+            }
+        }
+
+        stdout.write_all(b"\n]\n").await.unwrap();
+    }
+}
+
+
+pub struct Input{
+    starting_path: String,
+}
+
+pub struct Crawler{
+    elements_sender: UnboundedSender<Option<WeakFsElement>>,
+}
+
+pub type WriterElement = Weak<OwnedFsElement>;
+impl Crawler{
+    pub async fn crawl(&self, dir_path: &Path) -> Result<OwnedFsElement, anyhow::Error> {
+        async fn crawl_inner(dir_path: &Path, parent: Option<Weak<FsDir>>, sender: &UnboundedSender<Option<WeakFsElement>>) -> Result<Arc<FsDir>, anyhow::Error> {
+            let mut entries = tokio::fs::read_dir(dir_path).await?;
+
+            let dir = Arc::new(FsDir{
+                name: dir_path.file_name().unwrap_or(dir_path.as_os_str()).to_string_lossy().into_owned(),
+                r#type: FsFileType::Directory,
+                children: RwLock::new(Vec::new()),
+                parent,
+            }); 
+
+            let dir_cell = Arc::downgrade(&dir);
+            
+            while let Some(entry) = entries.next_entry().await?{
+                let open_entry_task = async {
+                    let entry_type = entry.file_type().await?;
+                    let entry_name = entry.file_name();
+                    let entry = {
+                        if entry_type.is_dir(){
+                            let dir = Box::pin(crawl_inner(entry.path().as_path(), Some(dir_cell.clone()), sender)).await?;
+                            OwnedFsElement::Dir(dir) 
+                        } else {
+                            let metadata = entry.metadata().await?;
+                            OwnedFsElement::Leaf( Arc::new( FsLeaf{ 
+                                name: entry_name.to_string_lossy().into_owned(),
+                                size: metadata.len() as usize,
+                                r#type: entry_type.into(),
+                                parent: Some(dir_cell.clone()),
+                            } ) )// handle file
+                        } 
+                    };
+                    Result::<_,anyhow::Error>::Ok(entry)
+                };
+                if let Ok(entry) =  open_entry_task.await{
+                    let weak_entry = entry.downgrade();
+                    sender.send(Some(weak_entry)).unwrap(); 
+                    dir.children.write().await.push(entry);
+                } else {
+                    continue;
+                }
+            }
+            Ok(dir) 
+        }
+        let owned_dir = crawl_inner(dir_path, None, &self.elements_sender).await?;
+        self.elements_sender.send(None).unwrap();
+        Ok(OwnedFsElement::Dir(owned_dir))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+struct OutputRow{
+    path: PathBuf,
+    r#type: FsFileType, 
+}
+pub struct Writer{
+    input: UnboundedReceiver<Option<WeakFsElement>>,
+}
+
+#[derive(Debug,)]
+pub enum OwnedFsElement{
+    Dir(Arc<FsDir>),
+    Leaf(Arc<FsLeaf>),
+}
+
+impl OwnedFsElement{
+    pub fn downgrade(&self) -> WeakFsElement{
+        match self{
+            OwnedFsElement::Dir(dir) => WeakFsElement::Dir(Arc::downgrade(dir)),
+            OwnedFsElement::Leaf(leaf) => WeakFsElement::Leaf(Arc::downgrade(leaf)),
+        }
+    }
+}
+impl WeakFsElement{
+    pub fn upgrade(&self) -> Option<OwnedFsElement>{
+        match self{
+            WeakFsElement::Dir(dir) => dir.upgrade().map(OwnedFsElement::Dir),
+            WeakFsElement::Leaf(leaf) => leaf.upgrade().map(OwnedFsElement::Leaf),
+        }
+    }
+}
+
+#[derive(Debug,)]
+pub enum WeakFsElement{
+    Dir(Weak<FsDir>),
+    Leaf(Weak<FsLeaf>),
+}
+
+pub type Parent = Weak<FsDir>;
+
+#[derive(Debug,)]
+pub struct FsDir{
+    pub name: String,
+    pub r#type: FsFileType,
+    pub children: RwLock<Vec<OwnedFsElement>>,
+    pub parent: Option<Weak<FsDir>>,
+}
+
+#[derive(Debug,)]
+pub struct FsLeaf{
+    pub name: String,
+    pub r#type: FsFileType,
+    pub size: usize,
+    pub parent: Option<Weak<FsDir>>,
+}
+
+
+impl HasBaseFsData for OwnedFsElement{
+    fn file_type(&self) -> FsFileType {
+        match self{
+            OwnedFsElement::Dir(dir) => dir.file_type(),
+            OwnedFsElement::Leaf(leaf) => leaf.file_type(),
+        }
+    }
+    fn name(&self) -> &str {
+        match self{
+            OwnedFsElement::Dir(dir) => dir.name(),
+            OwnedFsElement::Leaf(leaf) => leaf.name(),
+        }
+    }
+    fn parent(&self) -> Option<Weak<FsDir>> {
+        match self{
+            OwnedFsElement::Dir(dir) => dir.parent(),
+            OwnedFsElement::Leaf(leaf) => leaf.parent(),
+        }
+    }
+}
+
+
+impl HasBaseFsData for FsLeaf{
+    fn file_type(&self) -> FsFileType {
+        self.r#type.clone()
+    }
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn parent(&self) -> Option<Weak<FsDir>> {
+        self.parent.clone()
+    }
+}
+impl HasBaseFsData for FsDir{
+    fn file_type(&self) -> FsFileType {
+        self.r#type.clone()
+    }
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn parent(&self) -> Option<Weak<FsDir>> {
+        self.parent.clone()
+    }
+}
+
+trait HasBaseFsData{
+    fn file_type(&self) -> FsFileType;
+    fn name(&self) -> &str;
+    fn parent(&self) -> Option<Weak<FsDir>>;
+
+    fn make_row(&self) -> OutputRow where Self:Sized{
+        let path = {
+            let mut path = PathBuf::new();
+            let mut walker = self.path_walker();
+            while let Some(part) = walker.next(){
+                path.push(part);
+            }
+            path
+        };
+        OutputRow{
+            path,
+            r#type: self.file_type(),
+        }
+    }
+    fn path_walker(&self) -> FsElementParentPathIter<'_, Self> where Self: Sized {
+        FsElementParentPathIter::new(self)
+    }
+}
+
+struct FsElementParentPathIter<'a, Base: HasBaseFsData>{
+    stack: VecDeque<Arc<FsDir>>,
+    start: &'a Base,
+    index: usize,
+}
+
+
+impl<'a, Base: HasBaseFsData> FsElementParentPathIter<'a, Base>{
+
+    fn new(start: &'a Base) -> Self{
+        let mut stack = VecDeque::new();
+        let mut current = start.parent();
+        while let Some(weak_dir) = current{
+            if let Some(dir) = weak_dir.upgrade(){
+                stack.push_front(dir.clone());
+                current = dir.parent();
+            } else {
+                break;
+            }
+        }
+        Self{
+            stack,
+            start,
+            index: 0,
+        }
+    }
+
+    fn next<'b>(&'b mut self) -> Option<&'b str>{ 
+        match self.index {
+            i if i < self.stack.len() => {
+                let el = self.stack.get(i).unwrap();
+                self.index += 1;
+                Some(&el.name)
+            },
+            i if i == self.stack.len() => {
+                self.index += 1;
+                Some(self.start.name())
+            },
+            _ => None,
+        }
+    }
+}
+
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum FsFileType{
+    File,
+    Directory,
+    SymlinkFile,
+    SymlinkDir,
+    NoIdeaLmao,
+}
+
+impl Into<FsFileType> for FileType{
+    fn into(self) -> FsFileType {
+        if self.is_dir(){
+            FsFileType::Directory
+        } else if self.is_file(){
+            FsFileType::File
+        } else if self.is_symlink_file(){
+            FsFileType::SymlinkFile
+        } else if self.is_symlink_dir() {
+            FsFileType::SymlinkDir
+        } else {
+            FsFileType::NoIdeaLmao
+        }
+    }
+}
